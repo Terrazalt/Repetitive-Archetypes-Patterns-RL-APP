9f5b45a970404ff3312864d744a6f03927ee5ede
terrazalt <felipe.terraza@sansano.usm.cl>
2025-04-13 16:05:29 -0400

    boton de redes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 
 	let open: boolean = false;
+	let openCNN: boolean = false;
 	let icons: string[] = [];
 	let selectedImage: string | null = null;
 
@@ -46,10 +47,29 @@
 <header class="bg-amber-600 p-4 text-white">
 	<div class="relative container mx-auto flex items-center justify-between">
 		<h1 class="text-xl font-bold">Repetitive Archetypes Patterns Detector</h1>
+		<button on:click={() => (openCNN = !openCNN)}>
+			<span class="text-2xl">☰</span>
+		</button>
+		{#if openCNN}
+			<div
+				class="absolute right-0 z-10 mt-2 w-29 rounded bg-white text-black shadow-lg"
+				style="top: 100%;"
+			>
+				<button class="py2 block px-4 hover:bg-amber-400" on:click={() => alert('CNN Model')}
+					>CNN Model</button
+				>
+				<button class="py2 block px-4 hover:bg-amber-400" on:click={() => alert('CNN Model')}
+					>Yolo Model</button
+				>
+				<button class="py2 block px-4 hover:bg-gray-100" on:click={() => (openCNN = false)}>
+					Close
+				</button>
+			</div>
+		{/if}
 	</div>
 </header>
 
-<main class="flex flex-col items-center justify-center py-10 space-y-6">
+<main class="flex flex-col items-center justify-center space-y-6 py-10">
 	<!-- Imagen seleccionada o archivo -->
 	{#if selectedImage}
 		<img src={selectedImage} alt="Selected preview" class="mb-4 h-auto w-64 rounded shadow" />
@@ -62,7 +82,7 @@
 		type="file"
 		accept="image/*"
 		on:change={handleFileChange}
-		class="mb-6 px-4 py-2 rounded border"
+		class="mb-6 rounded border px-4 py-2"
 	/>
 
 	<!-- Botón de acción -->
@@ -72,20 +92,18 @@
 	>
 		Scan Image
 	</button>
+	<!-- Boton de seleccion de patron-->
 
-	<div class="relative">
-		<button
-			class="rounded bg-white px-4 py-2 text-black"
-			on:click={() => (open = !open)}
-		>
+	<div class="relative flex justify-end">
+		<button class="rounded bg-white px-4 py-2 text-black" on:click={() => (open = !open)}>
 			Seleccionar Imagen
 		</button>
 		{#if open}
 			<div
-				class="absolute left-0 z-10 mt-2 w-48 rounded bg-white text-black shadow-lg dropdown-menu"
+				class="dropdown-menu absolute right-0 z-10 mt-2 w-48 rounded bg-white text-black shadow-lg"
 				role="menu"
 				tabindex="0"
-				style="top: 100%;"
+				style="top: 90%;"
 			>
 				{#each icons as icon}
 					<button
@@ -95,13 +113,7 @@
 						role="menuitem"
 						tabindex="0"
 					>
-						<img
-							src={icon}
-							alt="icon option"
-							class="image-option"
-							width="100"
-							height="100"
-						/>
+						<img src={icon} alt="icon option" class="image-option" width="100" height="100" />
 					</button>
 				{/each}
 			</div>
